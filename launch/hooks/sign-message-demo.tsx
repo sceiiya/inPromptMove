fd2a594bcc604e4a9319d5c22619f4d7c74fedba
sceiiya <meetscheidj@gmail.com>
2025-06-13 13:27:47 +0800

    hide sample

--- launch/hooks/sign-message-demo.tsx
+++ launch/hooks/sign-message-demo.tsx
@@ -89,45 +89,54 @@
 
 
 
-"use client"
-import React from 'react';
-import { useWallet } from '@aptos-labs/wallet-adapter-react'; 
-import { Aptos, AptosConfig, Network } from '@aptos-labs/ts-sdk';
-import { MODULE_ADDRESS } from '@/constants';
+// "use client"
+// import React from 'react';
+// import { useWallet } from '@aptos-labs/wallet-adapter-react'; 
+// import { Aptos, AptosConfig, Network } from '@aptos-labs/ts-sdk';
+// import { MODULE_ADDRESS } from '@/constants';
  
-const config = new AptosConfig({ network: Network.TESTNET });
-const aptos = new Aptos(config);
+// const config = new AptosConfig({ network: Network.TESTNET });
+// const aptos = new Aptos(config);
  
-const SignAndSubmit = () => {
-  const { account, signAndSubmitTransaction } = useWallet();
+// const SignAndSubmit = () => {
+//   const { account, signAndSubmitTransaction } = useWallet();
  
-  const onSignAndSubmitTransaction = async () => {
-    if(account == null) {
-        throw new Error("Unable to find account to sign transaction")
-    }
-    const response = await signAndSubmitTransaction({
-      sender: account.address,
-      data: {
-        function: `${MODULE_ADDRESS}::add_self_to_whitelist`,
-        functionArguments: []
-      },
-    });
+//   const onSignAndSubmitTransaction = async () => {
+//     if(account == null) {
+//         throw new Error("Unable to find account to sign transaction")
+//     }
+//     const response = await signAndSubmitTransaction({
+//       sender: account.address,
+//       data: {
+//         function: `${MODULE_ADDRESS}::add_self_to_whitelist`,
+//         functionArguments: []
+//       },
+//     });
 
-    console.log(response)
-    // if you want to wait for transaction
-    try {
-      let txn = await aptos.waitForTransaction({ transactionHash: response.hash });
-      console.log(txn)
-    } catch (error) {
-      console.error(error);
-    }
-  };
+//     console.log(response)
+//     // if you want to wait for transaction
+//     try {
+//       let txn = await aptos.waitForTransaction({ transactionHash: response.hash });
+//       console.log(txn)
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
  
+//   return (
+//     <button className="py-1 px-3 border-1 border-white rounded-md bg-[#dbc4da]" onClick={onSignAndSubmitTransaction}>
+//       Whitelist Me!
+//     </button>
+//   );
+// };
+ 
+// export default SignAndSubmit;
+
+
+import React from 'react'
+
+export default function SignMessageDemo() {
   return (
-    <button className="py-1 px-3 border-1 border-white rounded-md bg-[#dbc4da]" onClick={onSignAndSubmitTransaction}>
-      Whitelist Me!
-    </button>
-  );
-};
- 
-export default SignAndSubmit;
+    <div>sign-message-demo</div>
+  )
+}
